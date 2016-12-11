0a25a03ecbf53723c30305d3eb64ef01b9b0fa6a
Albert Manya <albert.manya@schibsted.com>
2016-12-11 20:43:21 +0100

    Add initial screen

--- static/media/js/app.js
+++ static/media/js/app.js
@@ -1,63 +1,28 @@
-var timerExecuted = false;
-var editor = null;
+function makeid() {
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < 4; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
 
 $(document).ready(function() {
-
-    // Support TLS-specific URLs, when appropriate.
-    if (window.location.protocol == "https:") {
-      var ws_scheme = "wss://";
-    } else {
-      var ws_scheme = "ws://"
-    };
-
-    var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
-    var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit");
-
-    inbox.onmessage = function(message) {
-        var data = JSON.parse(message.data);
-        if("error" in data) {
-            $("#error").fadeIn("slow");
-            $("#error_msg").html("<strong>" + data.error + "</strong>");
-            setTimeout(function(){
-                $("#error").fadeOut("slow");
-            }, 10000);
-        } else {
-            GameInfo.command_buffer[data.handle].push(data);
+    var game_id = makeid();
+    $("#game-id").attr('placeholder', game_id);
+    var base_url = window.location.href;
+    var base_url_to_show = base_url.replace(/^https?\:\/\//i, "");
+    base_url = base_url.replace(/\/.*$/, "/");
+    base_url_to_show = base_url_to_show.replace(/\/.*$/, "/");
+    $("#game-url").text(base_url_to_show);
+    $("#start-form input[type=submit]").click(function(e){
+        e.preventDefault();
+        var game_id = $("#game-id").val();
+        if(!game_id) {
+            game_id = $("#game-id").attr('placeholder');
         }
-    };
-
-    editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-        lineNumbers: true,
-        tabSize: 4,
-        indentUnit: 4,
-        mode: "javascript"
+        var url = base_url + game_id;
+        window.location.replace(url);
     });
-    var original_code = "function main(){\n\n}";
-    var play_btn = document.getElementById('execute_btn');
-    play_btn.onclick = function() {
-        try {
-          var handle = $('input[name=handle]:checked').attr('id')
-          var text = editor.getValue();
-          outbox.send(JSON.stringify({ handle: handle, text: text }));
-        }
-        catch(e) {
-          $("#error").fadeIn("slow");
-          $("#error_msg").html("<strong>" + e.message + "</strong>");
-            setTimeout(function(){
-              $("#error").fadeOut("slow");
-            }, 10000);
-        }
-    };
-    var clean_btn = document.getElementById('clean');
-    clean_btn.onclick = function() {
-        var button = this;
-        bootbox.confirm("Estàs segur que vols netejar tot el que hi ha escrit a l'editor?", "Cancel·la", "Neteja", function(result){
-            if(result){
-                editor.setValue(original_code);
-            }
-        });
-    };
-
-
 });
-
---
+++ static/media/js/game.js
@@ -0,0 +1,68 @@
+var timerExecuted = false;
+var editor = null;
+
+$(document).ready(function() {
+
+    var karel_selector = $('input[name=handle]');
+    karel_selector.change(function(){
+
+    });
+
+    // Support TLS-specific URLs, when appropriate.
+    if (window.location.protocol == "https:") {
+      var ws_scheme = "wss://";
+    } else {
+      var ws_scheme = "ws://"
+    };
+
+    var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
+    var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit");
+
+    inbox.onmessage = function(message) {
+        var data = JSON.parse(message.data);
+        if("error" in data) {
+            $("#error").fadeIn("slow");
+            $("#error_msg").html("<strong>" + data.error + "</strong>");
+            setTimeout(function(){
+                $("#error").fadeOut("slow");
+            }, 10000);
+        } else {
+            GameInfo.command_buffer[data.handle].push(data);
+        }
+    };
+
+    editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+        lineNumbers: true,
+        tabSize: 4,
+        indentUnit: 4,
+        mode: "javascript"
+    });
+    var original_code = "function main(){\n\n}";
+    var play_btn = document.getElementById('execute_btn');
+    play_btn.onclick = function() {
+        try {
+          var handle = $('input[name=handle]:checked').attr('id')
+          var text = editor.getValue();
+          outbox.send(JSON.stringify({ handle: handle, text: text }));
+        }
+        catch(e) {
+          $("#error").fadeIn("slow");
+          $("#error_msg").html("<strong>" + e.message + "</strong>");
+            setTimeout(function(){
+              $("#error").fadeOut("slow");
+            }, 10000);
+        }
+    };
+    var clean_btn = document.getElementById('clean');
+    clean_btn.onclick = function() {
+        var button = this;
+        bootbox.confirm("Estàs segur que vols netejar tot el que hi ha escrit a l'editor?", "Cancel·la", "Neteja", function(result){
+            if(result){
+                editor.setValue(original_code);
+            }
+        });
+    };
+
+
+});
+
